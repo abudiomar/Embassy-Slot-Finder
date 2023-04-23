757b2e164350b2101bc38836c3c9e71253860b3a
Abdullah <abudi4947@gmail.com>
2023-04-23 17:41:54 +0300

    first commit

---
+++ emails/emails.js
@@ -0,0 +1,12 @@
+module.exports = [
+    { user: "enatschool1@gmail.com", pass: "Muazmusa@123" },
+    { user: "yishakdawit96@gmail.com", pass: "Muazmusa@123" },
+    { user: "ozakbederu@gmail.com", pass: "Muazmusa@123" },
+    { user: "travelmuaz99@gmail.com", pass: "Muazmusa@123" }
+]
+
+
+// { user: "semirtravelopia@gmail.com", pass: "Muazmusa@123" },
+// { user: "abdizak234@gmail.com", pass: "Muazmusa@123" },
+// { user: "munibamusa22@gmail.com", pass: "Muazmusa@123" },
+// { user: "haseenmusa093@gmail.com", pass: "Muazmusa@123" }
